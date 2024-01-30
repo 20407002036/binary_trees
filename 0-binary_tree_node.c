a82f1b69bdab517a86b890b29c3a8b50f405ad80
Solomon <kyobenimaru001@gmail.com>
2024-01-30 16:58:57 +0300

    commit for betty corrections

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -9,19 +9,19 @@
  * Return: A pointer to created node
  */
 
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value){
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
 
-     binary_tree_t *node = malloc(sizeof(binary_tree_t));
-     
-  if (node == NULL)
-  return (NULL);
+binary_tree_t *node = malloc(sizeof(binary_tree_t));
 
-  
-  node->n = value;
-  node->parent = parent;
-  node->left = NULL;
-  node->right =NULL;
-  
+if (node == NULL)
+return (NULL);
 
-  return (node);
+
+node->n = value;
+node->parent = parent;
+node->left = NULL;
+node->right = NULL;
+
+return (node);
 }
