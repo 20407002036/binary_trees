8acf89434a3fc56b74428c2ae614d3c077d80cff
Solomon <kyobenimaru001@gmail.com>
2024-02-01 09:05:52 +0300

    commit for the betty corrections

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -6,28 +6,28 @@
  * binary_tree_insert_left - inserts a left child node
  * @parent: This is the parent node for the currenr
  * @value: value at this node
- * 
+ *
  * Return: pointer to left node
-*/
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value){
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
 
-    if (parent == NULL){
-        return(NULL);
-    }
+	if (parent == NULL)
+		return (NULL);
 
- binary_tree_t *new_node = binary_tree_node(parent, value);
+	binary_tree_t *new_node = binary_tree_node(parent, value);
 
-    if (new_node == NULL)
-        return (NULL);
+	if (new_node == NULL)
+		return (NULL);
 
-    if (parent->left != NULL)
-    {
-        new_node->left = parent->left;
-        new_node->left->parent = new_node;
-    }
+	if (parent->left != NULL)
+	{
+		new_node->left = parent->left;
+		new_node->left->parent = new_node;
+	}
 
-    parent->left = new_node;
+	parent->left = new_node;
 
 
-    return (new_node);
+	return (new_node);
 }
--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -10,21 +10,21 @@
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-    if (parent == NULL)
-        return (NULL);
+	if (parent == NULL)
+		return (NULL);
 
-    binary_tree_t *new_node = binary_tree_node(parent, value);
+	binary_tree_t *new_node = binary_tree_node(parent, value);
 
-    if (new_node == NULL)
-        return (NULL);
+	if (new_node == NULL)
+		return (NULL);
 
-    if (parent->right != NULL)
-    {
-        new_node->right = parent->right;
-        new_node->right->parent = new_node;
-    }
+	if (parent->right != NULL)
+	{
+		new_node->right = parent->right;
+		new_node->right->parent = new_node;
+	}
 
-    parent->right = new_node;
+	parent->right = new_node;
 
-    return (new_node);
+	return (new_node);
 }
